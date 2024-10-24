90fa270543b4ecbee6aafaffdb874ac0ca14bf6f
Nuh Yiğit Akman <akmannuhyigit@gmail.com>
2024-10-25 01:03:56 +0300

    Add: Plane-Related Tasks

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,29 +1,60 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
+    public float speed = 10f;
+    public float rotationSpeed = 100f;
     public float verticalInput;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public float horizontalInput;
+    
+    private bool isEPressed;
+    private bool isFPressed;
 
     // Update is called once per frame
-    void FixedUpdate()
+    private void Update()
     {
-        // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
+        // Get the user's input for movement and rotation
+        verticalInput = Input.GetAxis("Vertical"); // For forward/backward movement
+        horizontalInput = Input.GetAxis("Horizontal"); // For left/right movement
+        
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            isEPressed = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.E))
+        {
+            isEPressed = false;
+        }
+        
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            isFPressed = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.F))
+        {
+            isFPressed = false;
+        }
+    }
 
-        // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+    private void FixedUpdate()
+    {
+        // Move the plane forward/backward
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime * verticalInput));
 
-        // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        // Move the plane right/left
+        transform.Translate(Vector3.right * (speed * Time.deltaTime * horizontalInput));
+
+        // Tilt the plane only when pressing E (tilt up) or F (tilt down)
+        if (isEPressed)
+        {
+            transform.Rotate(Vector3.right * (-rotationSpeed * Time.deltaTime)); // Tilt up (E key)
+        }
+        else if (isFPressed)
+        {
+            transform.Rotate(Vector3.right * (rotationSpeed * Time.deltaTime)); // Tilt down (F key)
+        }
     }
 }
